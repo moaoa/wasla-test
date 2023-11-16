e93fba3ea4b98cb88e01d2657d932c0ad801d3cf
moaoa <moaadbn3@gmail.com>
2023-11-16 09:25:40 +0200

    last changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
 import axios from "axios";
 import { Device } from "@twilio/voice-sdk";
+import { useLocalStorage } from "@vueuse/core";
 
 defineProps<{ msg: string }>();
 
-const filters = reactive({
+const filters = useLocalStorage("filters", {
   baseUrl: "https://1162-165-16-112-159.ngrok-free.app",
 
   phone: "+218920920110",
@@ -14,40 +14,45 @@ const filters = reactive({
   personToCallName: "",
 });
 
-const myName = ref("");
-const token = ref(localStorage.getItem("token") || "");
-const twilioToken = ref(localStorage.getItem("twilioToken") || "");
+const myName = useLocalStorage("myName", "");
+const token = useLocalStorage("token", "");
+const twilioToken = useLocalStorage("twilioToken", "");
 
 let device: any;
 
 const login = async () => {
   try {
-    const res = await axios.post(`${filters.baseUrl}/api/v1/user/login`, {
-      phone: filters.phone,
-      password: filters.password,
+    const res = await axios.post(`${filters.value.baseUrl}/api/v1/user/login`, {
+      phone: filters.value.phone,
+      password: filters.value.password,
     });
-    localStorage.setItem("token", res.data.token);
-    localStorage.setItem("twilioToken", res.data.twilio_token);
 
     token.value = res.data.token;
-    twilioToken.value = res.data.twilio_token;
 
-    device = new Device(twilioToken.value);
+    twilioToken.value = await getTwilioToken(res.data.token);
 
-    device.on("ready", () => {
-      console.log("Device is ready");
+    device = new Device(twilioToken.value, { logLevel: "debug" });
+
+    device.on(Device.EventName.Registered, () => {
+      console.log("Device is registered");
     });
 
-    device.on("error", (error: any) => {
+    device.on(Device.EventName.Error, (error: any) => {
       console.error("Error:", error);
     });
 
-    device.on("incoming", (connection: any) => {
+    device.on(Device.EventName.Incoming, (connection: any) => {
       console.log("Incoming connection:", connection);
     });
 
+    device.on(Device.EventName.TokenWillExpire, async () => {
+      // const newToken = await getAccessToken();
+      // device.updateToken(newToken);
+      console.log("token will expire");
+    });
+
     const res2 = await axios.post(
-      `${filters.baseUrl}/api/v1/general/client-id`,
+      `${filters.value.baseUrl}/api/v1/general/client-id`,
       null,
       {
         headers: {
@@ -55,10 +60,26 @@ const login = async () => {
         },
       }
     );
-    myName.value = res.data;
-    console.log("me: ", res2.data);
 
+    myName.value = res2.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTwilioToken = async (token: string) => {
+  try {
+    const res = await axios.post(
+      `${filters.value.baseUrl}/api/v1/general/generate-twilioToken`,
+      null,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     console.log(res.data);
+    return res.data.twilio_token;
   } catch (error) {
     console.log(error);
   }
@@ -68,16 +89,15 @@ const callClient = async () => {
   console.log(token.value);
 
   const callResponse = await axios.post(
-    `${filters.baseUrl}/api/v1/general/make-call-by-client`,
-    {
-      to: filters.personToCallName,
-    },
+    `${filters.value.baseUrl}/api/v1/general/make-call-by-client?to=${filters.value.personToCallName}`,
+    null,
     {
       headers: {
         Authorization: `Bearer ${token.value}`,
       },
     }
   );
+
   console.log(callResponse);
 
   // Add event listeners for device events
@@ -85,18 +105,33 @@ const callClient = async () => {
   // Make a call
   const params = {
     // To: "client:" + filters.personToCallName,
-    To: filters.personToCallName,
+    to: filters.value.personToCallName,
+    From: myName.value,
   };
 
   console.log("calling: ", device);
   console.log(params);
-  device.connect(params);
+
+  const connection = device.connect(params).then((res: any) => {
+    console.log("from promise: ", res);
+  });
+
+  console.log(connection);
+
+  // connection.on(Device.EventName.Incoming, () => {
+  //   console.log("incoming from connection");
+  // });
+
+  // connection.on(Device.EventName.Error, () => {
+  //   console.log("error from connection");
+  // });
 };
 </script>
 
 <template>
   <div style="margin-bottom: 5rem">
     <h1>login</h1>
+    <h1 style="color: red">{{ myName }}</h1>
     <div>
       <label>url</label>
       <br />
@@ -106,8 +141,12 @@ const callClient = async () => {
       <label>phone</label>
       <br />
       <select v-model="filters.phone">
-        <option value="+218920920110">+218920920110</option>
-        <option value="+218912245321">+218912245321</option>
+        <option value="+218920920110" style="padding: 16px">
+          +218920920110
+        </option>
+        <option value="+218912245321" style="padding: 16px">
+          +218912245321
+        </option>
       </select>
     </div>
     <div>
